070a9b1d898f3a6947a0ea9777dc21a5b647ff85
Usharbi Sin <C_Junjie@outlook.com>
2026-05-28 09:14:08 +0800

    Edited neovim config.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt
 opt.number = true          -- 显示行号
 opt.relativenumber = false -- 不显示相对行号
-opt.cursorline = false     -- 当前行不加下划线
+opt.cursorline = true      -- 当前行突出显示
 opt.wrap = false           -- 不自动换行
 opt.showcmd = true         -- 显示执行的命令
 opt.wildmenu = true        -- 命令补全菜单
@@ -18,7 +18,7 @@ opt.shiftwidth = 2         -- 设置代码位移和自动缩进时的长度
 opt.softtabstop = 2        -- 设置缩进长度
 opt.list = true            -- 显示行尾的空格
 opt.listchars = { tab = '▸ ', trail = '·', extends = '❯', precedes = '❮', nbsp = '×' }
-opt.textwidth = 0          -- 关闭自动换行
+opt.textwidth = 1          -- 开启自动换行
 opt.indentexpr = ''        -- 关闭智能缩进表达式
 opt.backspace = { 'indent', 'eol', 'start' }
 opt.foldmethod = 'indent'  -- 代码折叠
