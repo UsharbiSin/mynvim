91116e185b1b1b78d1c1c3d894934f469a313950
Usharbi Sin <C_Junjie@outlook.com>
2026-08-03 01:06:17 +0800

    修改了gitsigns预览当前块差异的快捷键

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -11,6 +11,7 @@ require('gitsigns').setup({
     end
 
     map('n', '<LEADER>hp', gs.preview_hunk, { desc = "预览当前块的差异" })
+    map('n', '<LEADER>ph', gs.preview_hunk, { desc = "预览当前块的差异" })
     map('n', '<LEADER>td', gs.toggle_deleted, { desc = "开关显示已删除行" })
   end
 })
