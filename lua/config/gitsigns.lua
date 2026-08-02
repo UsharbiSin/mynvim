746f2419696c822670aa28ffa69a9c050cdac3f8
C_Junjie@outlook.com <c_junjie@outlook.com>
2026-08-03 02:14:54 +0800

    修改了gitsigns的快捷键，添加了跳转前后块，修改了预览当前块差异的快捷键

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -10,7 +10,10 @@ require('gitsigns').setup({
       vim.keymap.set(mode, l, r, opts)
     end
 
-    map('n', '<LEADER>hp', gs.preview_hunk, { desc = "预览当前块的差异" })
+    map('n', '<LEADER>ph', gs.preview_hunk, { desc = "预览当前块的差异" })
     map('n', '<LEADER>td', gs.toggle_deleted, { desc = "开关显示已删除行" })
+    map('n', '<LEADER>rh', gs.reset_hunk, { desc = "回滚当前行" })
+    map('n', '<LEADER>[h', gs.prev_hunk, { desc = "跳转到上一个修改块" })
+    map('n', '<LEADER>]h', gs.next_hunk, { desc = "跳转到下一个修改块" })
   end
 })
